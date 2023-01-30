27bd531d983598e3f8c20e25776d46cbd15d8837
Artem <85881416+N0R1M@users.noreply.github.com>
2023-01-30 22:25:16 +0300

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot("5843870161:AAG8clSucD4H5tWVy7hVIo3y9j4kKJuuSZY")
-
-@bot.message_handler(commands=['start', 'help'])
-def greeting(message):
-    bot.reply_to(message, "Hello! It's a test!")
-bot.infinity_polling()
